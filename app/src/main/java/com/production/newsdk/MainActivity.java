9b840b84abd3aea71ddc7172ed505b576a281bcf
Bob <1712906@student.hcmus.edu.vn>
2020-09-21 04:16:40 +0700

    Test SDK

--- app/src/main/java/com/production/newsdk/MainActivity.java
+++ app/src/main/java/com/production/newsdk/MainActivity.java
@@ -1,5 +1,7 @@
 package com.production.newsdk;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try{
+            com.production.mylibrary.mylibrary.setAuher("com.production.newsdk");
+            com.production.mylibrary.mylibrary.setUri(getApplicationContext());
+            Intent intent =new Intent(this,com.production.mylibrary.mylibrary.class);
+            ComponentName componentName=new ComponentName(this,com.production.mylibrary.mylibrary.class);
+            intent.setComponent(componentName);
+            startActivity(intent);
+        }
+        catch (Exception e) {
+            System.out.println("Aaaaa"+e.toString());
+        }
     }
 }
